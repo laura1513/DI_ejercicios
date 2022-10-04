ddd76c8a767082fb6ed80209a2ee2a3a672baed4
laura <laurajorques2003@gmail.com>
2022-10-04 18:30:39 +0200

    Cmabiar el color del fondo a uno más 'chulo'

---
+++ tema02/prueba1/PrimeraVentana.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PrimeraVentana extends JFrame {
+    public PrimeraVentana() {
+        super("Mi primera Ventana");
+        setSize(300,100);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        this.getContentPane().setBackground(Color.orange);
+    }
+
+    public static void main(String[] args) {
+        new PrimeraVentana();
+    }
+}
