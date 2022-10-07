2ca6e4fea7e59a901fb0206fcdf4f129820c57dd
laura <laurajorques2003@gmail.com>
2022-10-07 16:21:29 +0200

    Prueba finalizada

---
+++ tema02/ventanaBoton/windowButton.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class windowButton extends JFrame {
+    private Container panel;
+
+    private JButton button1;
+
+    private JButton button2;
+    public windowButton() {
+        super("Window with button");
+        button1 = new JButton("Hello :)");
+        button2 = new JButton("Bye :(");
+        panel = getContentPane();
+        setLayout(new GridLayout(2, 1));
+        panel.add(button1);
+        panel.add(button2);
+        setSize(200, 100);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        windowButton app = new windowButton();
+    }
+
+}
