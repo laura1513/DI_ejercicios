c2ab8816ff4a9f887f85666cd70d4898ef2fd4b8
laura <laurajorques2003@gmail.com>
2022-10-14 17:33:46 +0200

    Creo proyecto

--- tema02/prueba3/ejercicio/prueba3.java
+++ tema02/prueba3/ejercicio/prueba3.java
@@ -4,7 +4,6 @@ import java.io.*;
         import java.util.ArrayList;
         import java.util.HashMap;
         import java.util.Scanner;
-
 public class prueba3 {
     public static class Persona implements Serializable {
 
