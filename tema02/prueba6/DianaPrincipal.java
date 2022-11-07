adb7b72671e0f01ee6b434d3577a21220d4eed21
laura <laurajorques2003@gmail.com>
2022-11-07 11:38:17 +0100

    Inicio practica 6 tema 2

--- tema02/prueba6/DianaPrincipal.java
+++ tema02/prueba6/DianaPrincipal.java
@@ -4,14 +4,18 @@ import java.awt.*;
 public class DianaPrincipal extends JFrame {
     public DianaPrincipal() {
         super("Diana Horizontal");
+        //Se utiliza para calcular las medidas de la pantalla
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
 
         JSlider diana = new JSlider(0,100);
 
         add(diana);
+        //Añado las dimensiones de la ventana teniendo en cuenta que queremos que la anchura sea el 50% de la pantalla
         setSize((int) screenSize.getWidth()*50/100, 100);
+        //Pongo la ventana en el centro de la pantalla
         setLocationRelativeTo(null);
+        //Al cerrar la ventana, el programa finalizará
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
